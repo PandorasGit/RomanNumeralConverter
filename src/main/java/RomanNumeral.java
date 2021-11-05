74c0ac9e57a577db33b2025f9aa0d58a43c92c15
wendallR <wendallar125@gmail.com>
2021-11-05 14:14:07 -0400

    Create Initial RomanNumeral data structure

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -1,2 +1,14 @@
 public class RomanNumeral {
+
+    private String numeralString;
+    private int numeralInt;
+    private RomanNumeral nextNumeral=null;
+
+    public RomanNumeral(String numeralString) {
+        this.numeralString = numeralString;
+    }
+
+    public RomanNumeral next() {
+        return nextNumeral;
+    }
 }
--- src/test/java/RomanNumeralTest.java
+++ src/test/java/RomanNumeralTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RomanNumeralTest {
+
+    @Test
+    public void testIfNextNumeralIsNull(){
+        RomanNumeral romanNumeral = new RomanNumeral("i");
+        RomanNumeral nextNumeral = romanNumeral.next();
+        Assertions.assertNull(nextNumeral);
+
+    }
 }
